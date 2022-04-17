4643d694353e32b5e302fe7c6a760d7b6098b76a
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-04-17 23:55:32 +0900

    Implement the Model object with the game functionality. The model object takes the user guess (still not implemented) and checks if it is a hit/miss, register the hit, set the hit/miss class and shows i the display message div.

--- battleship.js
+++ battleship.js
@@ -1,3 +1,4 @@
+// the view object controls the display div and add "hit" or "miss" classes to the td in the table.
 var view = {
     displayMessage: function(msg) {
         var messageArea = document.getElementById("messageArea");
@@ -13,17 +14,49 @@ var view = {
     }
 };
 
-var ships = [
-    {
-      locations: ["10", "20", "30"], 
-      hits: ["", "", ""]
-    },
-    {
-      locations: ["32", "33", "34"],
-      hits: ["", "", ""]
-    },
-    {
-      locations: ["63", "64", "65"],
-      hits: ["", "", ""]  
-    }
-]
+// The model object holds the ships locations and info about the ships like how many, length, sunk and board size.
+
+var model = { 
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships: [ { locations: ["10", "20", "30"], hits: ["", "", ""] },
+             { locations: ["32", "33", "34"], hits: ["", "", ""] },
+             { locations: ["63", "64", "65"], hits: ["", "", ""] } ],
+    fire: function(guess) {
+        for (var i = 0; i <this.numShips; i++)
+            {
+                var ship = this.ships[i];
+                var index = ship.locations.indexOf(guess);
+                if (index >= 0)
+                {
+                    ship.hits[index] = "hit";
+                    view.displayHit(guess);
+                    view.displayMessage("HIT!");
+                    if(this.isSunk(ship))
+                    {
+                        view.displayMessage("You sank my battleship!");
+                        this.shipsSunk++;
+                    }
+                    return true;
+                }
+            } 
+            view.displayMiss(guess);
+            view.displayMessage("You missed.");
+            return false;
+        },
+        isSunk: function(ship) 
+        {
+            for (var i = 0; i < this.shipLength; i++)
+            {
+                if (ship.hits[i] !== "hit") 
+                {
+                    return false;
+                }
+            }
+        return true;
+        }
+};
+
+model.fire("63");
