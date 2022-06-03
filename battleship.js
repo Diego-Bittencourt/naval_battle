ec343ffa462b1cbdd335cda634694cdee5adf911
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-06-03 15:43:17 +0900

    Fix input's case related bug

--- battleship.js
+++ battleship.js
@@ -163,7 +163,7 @@ function init() {
 
 function handleFireButton() {
   var guessInput = document.getElementById("guessInput");
-  var guess = guessInput.value;
+  var guess = guessInput.value.toUpperCase();
   controller.processGuess(guess);
   guessInput.value = "";
 }
